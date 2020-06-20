2df42598fc1d0fd8648bb1130807834e22d6c1d1
Kyle Hannon <khannon@cocc.edu>
2020-06-19 23:27:58 -0700

    Learning stuff

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Kyle</h1>
+        <p>Comp. Sci. Student at OSU-C</p>
+        <ol>
+          <li>Japan</li>
+          <li>Japan</li>
+          <li>Japan</li>
+        </ol>
+      </div>
+    )
+  }
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+import MyInfo from './components/MyInfo'
+
+ReactDOM.render(<MyInfo />, document.getElementById('root'))
