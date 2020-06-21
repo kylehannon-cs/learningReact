99f1689ea65fbcabcb1107ee1a73af4c54cd49a6
Kyle Hannon <khannon@cocc.edu>
2020-06-21 01:23:52 -0700

    Still learning Reactjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CheckBox from './components/CheckBox'
+
+function App(){
+    return(
+        <div>
+            <CheckBox />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/CheckBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/CheckBox.css'
+
+function CheckBox() {
+    return (
+        <div className = 'box'>
+            <input type='checkbox' />
+            <p>Placeholder text here</p>
+
+            <input type='checkbox' />
+            <p>Placeholder text here</p>
+
+            <input type='checkbox' /> 
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default CheckBox
